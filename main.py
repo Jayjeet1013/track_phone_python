8da947e88037c85585564685b05ab59e44abdcac
Jayjeet1013 <jayjeet1013@gmail.com>
2023-09-16 21:09:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import phonenumbers
+
+from test import number
+
+from phonenumbers import geocoder
+
+ch_nmber =phonenumbers.parse(number,"CH")
+
+print(geocoder.description_for_number(ch_nmber,"en"))
